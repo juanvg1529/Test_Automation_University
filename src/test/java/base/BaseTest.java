0373ce3eded533081a124334a695834ae9b5a92b
juandiegovg <67924398+juanvg1529@users.noreply.github.com>
2023-07-25 22:50:17 -0500

    EventListerners

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,12 +3,14 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventsRepoter;
 import utils.WindowManager;
 
 import java.io.File;
@@ -19,13 +21,17 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    private WebDriver driver;//1st steep create the web driver
+  /*  private EventFiringWebDriver driver;//1st steep create the web driver*/
+    WebDriver driver;
     protected HomePage homePage;
     @BeforeClass
     public void setUp()
     {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");//specify where the web-driver is located
+
         driver = new ChromeDriver();
+     /*   driver = new EventFiringWebDriver(  new ChromeDriver());
+        driver.register(new EventsRepoter());*/
 
        // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
